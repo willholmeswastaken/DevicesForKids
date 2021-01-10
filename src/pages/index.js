e2562148b1e60d1fa26485dbc1428beedaf0fd60
willholmeswastaken <willholmes97@icloud.com>
2021-01-10 02:29:31 +0000

    will this fix a device sizing issue... stay tuned

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,11 @@ const IndexPage = () => (
           We want to make sure no child is left out of learning during this lockdown. Too many children do not have access to a device to do online learning. Please donate whatever you can!
         </p>
         <div className="flex w-full justify-center md:justify-start lg:pb-0 fade-in ">
-          <a href="https://www.gofundme.com/f/help-keep-local-leicestershire-children-learning" className="flex bg-blue-500 rounded-full font-bold text-white px-4 py-3 bounce-top-icons hover:bg-blue-600 mr-6">
+        <button
+            class="inline-block px-6 py-2 font-medium leading-7 text-center text-white uppercase bounce-top-icons bg-blue-700 rounded-full shadow ripple hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+          >
             Donate
-            <svg xmlns="http://www.w3.org/2000/svg" className="inline ml-2 w-6 stroke-current text-white stroke-2" viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12" /><polyline points="12 5 19 12 12 19" /></svg>
-          </a>
+          </button>
           {/* <iframe src="https://www.gofundme.com/f/help-keep-local-leicestershire-children-learning/widget/small/" frameborder="0" width="50%" height="50px" scrolling="no"></iframe> */}
         </div>
 
