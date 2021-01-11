dfc60d12133ec2d8ecde29996b5b1b37c5902f66
willholmeswastaken <willholmes97@icloud.com>
2021-01-11 20:29:46 +0000

    Content change

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children }) => {
       <div>
         <div className="text-center flex flex-col p-4 md:text-left md:flex-row md:items-center md:justify-between md:p-12 bg-indigo-100 rounded-md">
           <div className="text-2xl font-semibold">
-            <div className="text-gray-900">Luke is on BBC Radio Leicester at 10:30am 11/01/2021</div>
+            <div className="text-gray-900">Luke is on BBC Radio 5 Live between 4-7pm 12/01/2021</div>
             <div className="text-purple-500">Tune in to hear what the latest is!</div>
           </div>
         </div>
